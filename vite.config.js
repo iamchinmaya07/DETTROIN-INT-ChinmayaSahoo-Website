5a98f89cbff085c6afe06654829aafd3d6c69e33
user.email <cs172668@gmail.com>
2026-07-26 00:26:46 +0530

    fix: inject React globally via esbuild config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic',
-    }),
-  ],
+  plugins: [react()],
   base: '/',
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
 });
